6ec7a5f173b471ee8d23988101eb8d73f7cad2f4
canzhang-klick <czhang@klick.com>
2013-09-12 11:26:51 -0400

    Parse the subtitle file into memory while checkout its format.

--- validator.py
+++ validator.py
@@ -1,27 +1,61 @@
+import re
+
 class Validator:
     def __init__(self, filename):
         self.filename = filename
+        self.line_numbers = []
+        self.timestamps = []
+        self.chinese_captions = []
+        self.english_captions = []
+        self.parsed = False
             
     
-    def line_format(self):
+    def parse_file(self):
         f = file(self.filename)
         
         line = f.readline()
         cur_line = 1
-        while line != None:
+        while line != "":
             if not line.strip().isdigit():
-                print "Line " + cur_line + ": Invalid subtitle index (" + line + ")"
+                print "Line " + str(cur_line) + ": Invalid subtitle index \nActual: " + line
                 return False
+            else:
+                self.line_numbers.append(line)
+
             
             line = f.readline()
+            cur_line += 1
             
+            timestamp_regex = re.compile(\
+            '(\d+):(\d+):(\d+),(\d+) --> (\d+):(\d+):(\d+),(\d+)')
             
+            timestamp_result = timestamp_regex.match(line)
+            if (not timestamp_result):
+                print "Line " + str(cur_line) + ": Invalid timestamp line \nActual: " + line
+                return False
+            else:
+                self.timestamps.append(line)
             
+            line = f.readline()
+            cur_line += 1
+            self.chinese_captions.append(line)
+            
+            line = f.readline()
+            cur_line += 1
+            self.english_captions.append(line)
+            
+            line = f.readline()
+            cur_line += 1
+            if line.strip() != "":
+                print "Line " + str(cur_line) + ": Empty line expected \nActual: " + line
+                return False
                 
-            
+            line = f.readline()
+            cur_line += 1
         
-    def whitespace(self):
-        
-        
-        return True
-    
+        self.parsed = True    
+        return True    
+
+if __name__ == "__main__":
+    v = Validator("step1.p2.done by.raitorm.srt")
+    print v.parse_file()
