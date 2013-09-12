e5d8eeac8f753d0818e1cfbd71925d00a8b59b61
canzhang-klick <czhang@klick.com>
2013-09-12 15:16:34 -0400

    Rename whitespace_check. Add checks for upper case letters in the English subtitles.

--- validator.py
+++ validator.py
@@ -8,6 +8,7 @@ class Validator:
         self.chinese_captions = []
         self.english_captions = []
         self.parsed = False
+        self.initial_upper_list = []
             
     
     def parse_file(self):
@@ -15,6 +16,8 @@ class Validator:
         
         line = f.readline()
         cur_line = 1
+        self.initial_upper_list.append(True);
+        
         while line != "":
             if not line.strip().isdigit():
                 print "Line " + str(cur_line) + ": Invalid subtitle index \nActual: " + line
@@ -43,6 +46,12 @@ class Validator:
             line = f.readline()
             cur_line += 1
             self.english_captions.append(line)
+            end_of_sentence_re = re.compile('.*[\\.\\?\\!]$');
+            end_of_sentence_result = end_of_sentence_re.match(line.strip())
+            if end_of_sentence_result:
+                self.initial_upper_list.append(True)
+            else:
+                self.initial_upper_list.append(False)
             
             line = f.readline()
             cur_line += 1
@@ -59,7 +68,7 @@ class Validator:
                 
             
         
-    def whitespace(self):
+    def whitespace_check(self):
         
         haserror = False
         
@@ -87,7 +96,26 @@ class Validator:
             index += 1
 
         return not haserror
-			
+	
+    def upper_case_check(self):
+        
+        index = 0
+        haserror = False
+        for line in self.english_captions:
+            first_letter = line.strip()[0]
+            if self.initial_upper_list[index]:
+                if first_letter.islower():
+                    haserror = True
+                    if index == 0:
+                        print str(index + 1) + ": Expect upper case letter\nActual: " +\
+                        line.strip() + "\nPrevious: **This is the first line of the text**"
+                    else:
+                        print str(index + 1) + ": Expect upper case letter\nActual: " +\
+                        line.strip() + "\nPrevious: " + self.english_captions[index - 1]
+            
+            index += 1
+        return not haserror    
+    
 def find_whitespace_right(line):
         
     right_whitespace_re = re.compile('^.*[ \t\f\v]+$')
@@ -98,4 +126,5 @@ def find_whitespace_right(line):
 if __name__ == "__main__":
     v = Validator("step1.p2.done by.raitorm.srt")
     print v.parse_file()
-    print v.whitespace()
+    print v.whitespace_check()
+    print v.upper_case_check()
