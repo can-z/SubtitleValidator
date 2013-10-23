9f7e50e8b93fbe5e9d98138bb82681b2f4de702e
canzhang-klick <czhang@klick.com>
2013-10-23 17:37:29 -0400

    Add logging for local runner for unhandled exceptions. The subtitle file will be copied and stored in a folder.

--- local_runner.py
+++ local_runner.py
@@ -1,9 +1,30 @@
 import Tkinter
 import tkFileDialog
 import sys
+import datetime
+import shutil
+import os
+import traceback
 from src import validator
 
+LOG = False
+
+
+def error_logger(type, value, tb):
+    shutil.copy(trouble_maker, "troublemaker/" + os.path.basename(trouble_maker))
+    for line in traceback.format_exception(type, value, tb):
+        print line
+
 if __name__ == "__main__":
+
+    if LOG:
+        sys.excepthook = error_logger
+        log = open("log/" + datetime.datetime.now().strftime("%Y%m%d") + ".log", "a")
+        sys.stdout = log
+        sys.stderr = log
+
+        log.write("*********************\n*")
+        log.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "*\n*********************\n")
 
     root = Tkinter.Tk()
     root.withdraw()
@@ -12,7 +33,14 @@ if __name__ == "__main__":
 
     if not f:
         sys.exit(0)
+    global trouble_maker
+    trouble_maker = f
+
+    if LOG:
+        log.write("File name: " + f.encode("utf8", errors="ignore") + "\n\n")
+
     v = validator.Validator(f, True)
+
     if not v.parse_file():
         v.produce_result_file(True)
         sys.exit(-1)
