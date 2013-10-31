0e34d77321c19eadf4ec3d74a5f292dc2dc37253
canzhang-klick <czhang@klick.com>
2013-10-31 16:07:01 -0400

    Add logging on server.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+import shutil
 
 from flask import Flask, request, render_template, redirect, url_for
 from werkzeug import utils
@@ -33,14 +35,28 @@ def upload_file():
 def result(filename):
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     v = validator.Validator(full_filename, False)
-    if not v.parse_file():
-        res = v.produce_result_file(True)
-        return render_template("result.html", res=res)
-    v.perform_all_checks()
 
-    res = v.produce_result_file(False)
+    try:
+        if not v.parse_file():
+            res = v.produce_result_file(True)
+            return render_template("result.html", res=res)
+        v.perform_all_checks()
+        res = v.produce_result_file(False)
+    except:
+        logger.error("Error processing file: " + filename)
+        shutil.copy(full_filename, "trouble_maker/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "-" \
+                                   + os.path.basename(full_filename))
+        os.remove(full_filename)
+        raise
     os.remove(full_filename)
     return render_template("result.html", res=res)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    import logging
+    logging.basicConfig(filename='log/' + datetime.datetime.now().strftime("%Y%m%d") + ".log", \
+                        level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    stderr_handler = logging.StreamHandler()
+    logger.addHandler(stderr_handler)
+    app.run(debug=False)
